fa147c83812850db567a3f9681d612858153971c
ndeamador <ndeamador@gmail.com>
2021-01-16 23:38:24 +1100

    Added cypress plugin for eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,17 @@ module.exports = {
         "node": true,
         "commonjs": true,
         "es2021": true,
-        "jest/globals": true
+        "jest/globals": true,
+        "cypress/globals": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+      "eslint:recommended",
+      "plugin:cypress/recommended"
+    ],
     "parserOptions": {
         "ecmaVersion": 12
     },
     "rules": {
     },
-    "plugins": ["jest"]
+    "plugins": ["jest", "cypress"]
 };
